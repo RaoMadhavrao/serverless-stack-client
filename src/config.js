89a34dd0925050b9f34c0b60095e619c13406feb
Rao Madhavrao <lmadhavr@TDS-C02ZT3LTMD6P.local>
2020-05-15 08:16:40 -0700

    Changes to app

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "notes-app-uploads-rao"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://lebmtky6gc.execute-api.us-west-2.amazonaws.com/prod/"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_VPEXEFmrb",
+      APP_CLIENT_ID: "7scbbms2qlkupgu9f1iltt2ekl",
+      IDENTITY_POOL_ID: "us-west-2:7b9899ee-7859-4835-84aa-f158103ddac7"
+    }
+  };
+  
