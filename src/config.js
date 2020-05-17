ab17a9e3ce11f6490a259d3a1062e9b389b69893
Rao Madhavrao <lmadhavr@TDS-C02ZT3LTMD6P.local>
2020-05-17 01:42:14 -0700

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "pk_test_aWdJ6H3J8gH0l4J2H4YPXPDF00LWpVNbOO",
     s3: {
       REGION: "us-west-2",
       BUCKET: "notes-app-uploads-rao"
     },
     apiGateway: {
       REGION: "us-west-2",
-      URL: "https://lebmtky6gc.execute-api.us-west-2.amazonaws.com/prod/"
+      URL: "https://lebmtky6gc.execute-api.us-west-2.amazonaws.com/prod"
     },
     cognito: {
       REGION: "us-west-2",
